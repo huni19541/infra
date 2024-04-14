bc16e72aa591f9b9f0c0f6a97126458ec11f6d59
김동훈 <133645725+huni19541@users.noreply.github.com>
2024-04-14 23:07:25 +0900

    Add files via upload

---
+++ circuit-breaker/test/src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.test.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        log.info("test controller called");
+        return "test";
+    }
+}
